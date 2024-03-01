76aaf7e6b11c64be6f81cad74880aa3921d39149
Leon Jacobs <leonja511@gmail.com>
2024-03-01 09:21:23 +0200

    (feat) add a debug log helper

--- log.go
+++ log.go
@@ -1,6 +1,7 @@
 package trauth
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -11,4 +12,9 @@ func NewLogger() *log.Logger {
 	logger.SetOutput(os.Stdout)
 
 	return logger
+}
+
+// debugLog is a helper to print logs for debugging purposes
+func debugLog(m string) {
+	os.Stdout.WriteString(fmt.Sprintf(" --> [trauth debug] %s", m))
 }
